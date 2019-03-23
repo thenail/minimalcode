74b91cc885f7a106f26b4b3f43015e74bffec946
Kristian Nagel <kristian.nagel@gmail.com>
2019-03-23 20:29:10 +0100

    Reset tenant

--- Manage/ManageSiteCollectionAppCatalog.ps1
+++ Manage/ManageSiteCollectionAppCatalog.ps1
@@ -1,8 +1,6 @@
 . "$((Get-Location).Path)\..\Base\_Setup.ps1"
 Set-Log -FileName $MyInvocation.MyCommand.Name
 
-# $site = Get-SPOSite https://acandolab01.sharepoint.com/sites/cookbook2
-
-$site = Get-SPOSite https://inmotioncloud.sharepoint.com
+$site = Get-SPOSite https://yourtenant.sharepoint.com
 
 Add-SPOSiteCollectionAppCatalog -Site $site
